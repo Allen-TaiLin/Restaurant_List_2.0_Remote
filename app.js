6c140e05e4027e82b3659d358db77dbbe0f92ab6
Allen-TaiLin <skygary2000@gmail.com>
2020-11-12 21:46:21 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require packages used in the projects
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,20 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 // routes setting
 app.get('/', (req, res) => {
