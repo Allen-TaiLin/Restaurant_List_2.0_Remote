7809b25e2f58b9faae8d0afd8c9ddd0aa7e68079
Allen-TaiLin <skygary2000@gmail.com>
2020-11-13 01:01:50 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const RestaurantData = require('./models/restaurant.js') // 載入 restaurant model
 const app = express()
 const port = 3000
 
@@ -10,6 +12,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 // 取得資料庫連線狀態
@@ -27,8 +30,26 @@ db.once('open', () => {
 
 // routes setting
 app.get('/', (req, res) => {
-  //res.send('restaurant-list')
-  res.render('index')
+  RestaurantData.find()  // 取出 restaurant model 裡的所有資料
+    .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((restaurants) => res.render('index', { restaurants: restaurants }))  // 將資料傳給 index 樣板
+    .catch((error) => console.log(error))  // 錯誤處理
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  let restaurantList = []
+  RestaurantData.find()
+    .lean()
+    .then((restaurants) => {
+      restaurantList = restaurants.filter((item) => {
+        console.log(item.name)
+        return (item.name.toLowerCase().trim().includes(keyword.toLowerCase().trim())) || (item.category.toLowerCase().trim().includes(keyword.toLowerCase().trim()))
+      })
+
+      return res.render('index', { restaurants: restaurantList, keyword: keyword })
+    })
+    .catch((error) => console.log(error))
 })
 
 // start and listen on the Express server
